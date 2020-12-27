3534177d51a7da7809cdad64a7d203dcc0367aeb
Ligin Thomas CK <ciyakhan515@gmail.com>
2020-12-27 22:56:43 +0530

    Create configure.php

---
+++ configure.php
@@ -0,0 +1,10 @@
+$receiver = "receiver email address here";
+$subject = "Email Test via PHP using Localhost";
+$body = "Hi, there...This is a test email send from Localhost.";
+$sender = "From:sender email address here";
+
+if(mail($receiver, $subject, $body, $sender)){
+    echo "Email sent successfully to $receiver";
+}else{
+    echo "Sorry, failed while sending mail!";
+}
